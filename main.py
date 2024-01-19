ce53fc0a1e20f41587697b7672cd08341a79ae5d
Alexandro Hou <alexandro.hou@tamadia.ch>
2024-01-19 18:32:36 +0000

    connect by env var

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
 from pyathena import connect
-
+import os
 # Initialize connection to db
 
 @st.cache_resource
 def init_connection():
-    #url: str = st.secrets['supabase_url']
-    #key: str = st.secrets['supabase_key']
     #connection = connect(s3_staging_dir="s3://atommych-datalake-dev/staging/streamlit/", region_name="eu-west-3")
-    connection = connect(schema_name="datalake_raw", work_group="atommych-athena-workgroup-dev", region_name="eu-west-3")
+    #connection = connect(schema_name="datalake_raw", work_group="atommych-athena-workgroup-dev", profile_name="personal", region_name="eu-west-3")
+
+    # Get environment variables
+    access_key = os.getenv('access_key')
+    secret_key = os.environ.get('secret_key')
+    region = os.environ.get('region')
+    s3_staging_dir = os.environ.get('s3_staging_dir')
+    connection = connect(aws_access_key_id=secret_key,
+                     aws_secret_access_key=access_key,
+                     s3_staging_dir=s3_staging_dir,
+                     region_name=region)
     return connection
 
 # Query the db
